1a58b61f3151f20d7da62aba112f8f6d90acb401
“Eslam“ <aslamalmswdya@gmail.com>
2025-04-09 17:32:52 +0200

    Update Cart

--- backend/routes/cartRoute.js
+++ backend/routes/cartRoute.js
@@ -82,4 +82,58 @@ router.post("/", async (req, res) => {
     return res.status(500).json({ message: "Server error" });
   }
 });
+
+
+ 
+
+// @route PUT /api/cart
+// @desc Update product quantity in the cart for a guest or logged-in user
+// @access Public
+router.put("/", async (req, res) => {
+  const { productId, quantity, size, color, guestId, userId } = req.body;
+
+  try {
+    let cart = await getCart(userId, guestId);
+    if (!cart) return res.status(404).json({ message: "Cart not found" });
+
+    const productIndex = cart.products.findIndex(
+      (p) => p.productId.toString() === productId && p.size === size && p.color === color
+    );
+
+    if (productIndex === -1) {
+      return res.status(404).json({ message: "Product not found in cart" });
+    } 
+    if (quantity > 0) {
+      cart.products[productIndex].quantity = quantity;
+    } else { 
+      cart.products.splice(productIndex, 1);
+    }
+ 
+    cart.totalPrice = cart.products.reduce((acc, item) => {
+      return acc + item.price * item.quantity;
+    }, 0);
+ 
+    await cart.save();
+    
+    return res.status(200).json({
+      success: true,
+      cart
+    });
+  } catch (error) {
+    console.error("Cart update error:", error);
+    return res.status(500).json({ message: "Server error" });
+  }
+});
+
+
+
+
+
+
+
+
+
+
+
+
 module.exports = router;
